207f28c76a7bc59fe9956dfc8e44f4e41436f4d6
sarah almutari <sara0moha78@gmail.com>
2022-08-28 00:12:57 +0300

    .

--- scripte.js
+++ scripte.js
@@ -1,16 +1,1 @@
-//navbar
-
-window.onscroll = function() {stickynav()};
-    
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-  
-function stickynav() {
-if (window.pageYOffset >= sticky) {
-navbar.classList.add("sticky")
-} else {
-navbar.classList.remove("sticky");
-}
-}
-
 
